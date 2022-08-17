a3034f3ac422d0bc2f76d4da73c48f2d61b498fc
Demian Rosas <demianrosas@hotmail.com>
2022-08-17 11:50:54 -0300

    Setup passport

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,34 @@
 import express, { Application, Response } from 'express';
+import passport from 'passport';
+import { Strategy } from 'passport-http-bearer';
 import { etlPipeline } from './etlPipeline';
 
 const app: Application = express();
 
 const port = process.env.PORT || 4000;
+const validToken = process.env.VALID_TOKEN || 'my-valid-token';
 
-app.get('/breweries', async (_, res: Response) => {
-  try {
-    const data = await etlPipeline();
-    res.send(data);
-  } catch (err) {
-    res.status(500).send(err);
+passport.use(
+  new Strategy(function (token, done) {
+    if (token === validToken) {
+      return done(null, true);
+    }
+    return done(null, false);
+  })
+);
+
+app.get(
+  '/breweries',
+  passport.authenticate('bearer', { session: false }),
+  async (_, res: Response) => {
+    try {
+      const data = await etlPipeline();
+      res.send(data);
+    } catch (err) {
+      res.status(500).send(err);
+    }
   }
-});
+);
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
